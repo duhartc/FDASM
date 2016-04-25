6f2933a1a0561fd2045ea81202cda71dbc0fc7a7
Filipe Gomes <filipe.gomes@ensimag.grenoble-inp.fr>
2016-04-25 18:41:16 +0200

    squelette du main

---
+++ main.c
@@ -0,0 +1,24 @@
+int main (int argc , char * argv[]) {
+  //Initialiser la struct
+  InitialiserStruct();
+  
+  //Parser et remplir le tableau
+  file = Reuters blablaba;
+  ParseFichier(file);
+  
+  //Scinder aléatoirement en apprentissage et test 
+  //apprentissage 52 500 et test 18203
+  tabApprentissage = ...;
+  tabTest = ...;
+  ScinderCollection(struct, tabAprentissage, tabTest);
+
+  //Estimation paramètre
+  EstimationBernouillli();
+  EstimationMultidim();
+  
+  //Calcul test
+  int PBC[29];
+  PBC[classe]= CalculTest(..);
+
+  //Generaliser et boucler sur 20.
+}
