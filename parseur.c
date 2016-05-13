c7366c9bf4ada4df0df81c785e7c60df75e17191
Claudia D <claudiadhrt@hotmail.fr>
2016-05-13 18:20:24 +0200

    fonction pour scinder la structure

--- parseur.c
+++ parseur.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 struct cellule {
     int ind;
@@ -17,13 +18,21 @@ struct doc {
 };
 typedef struct doc doc;
 
+struct divided_collection {
+    doc *entrainement;
+    doc *test;
+};
+typedef struct divided_collection divided_collection;
+
 #define NB_DOC 70703 
 #define TAILLE_VOC 10000 //TODO: trouver réellement
+#define TAILLE_ENTRAINEMENT 52500
+#define TAILLE_TEST 18203
 
 doc tableau[NB_DOC]; // = malloc(70703*sizeof(cell)); 
 
-void affiche_tableau(doc * tab) {
-    for (int i = 0; i < NB_DOC; i++) {
+void affiche_tableau(doc * tab, int taille) {
+    for (int i = 0; i < taille; i++) {
         printf("%d ", tab[i].classe);
         cell* cour = tab[i].vect;
         while (cour != NULL) {
@@ -33,10 +42,41 @@ void affiche_tableau(doc * tab) {
         printf("\n");
     }
 }
+void melanger(int* tableau, int taille){
+    int nombre_tire=0;
+    int temp=0;
+    for(int i = 0; i< taille;i++){
+        nombre_tire=rand()%taille;
+        // On échange les contenus des cases i et nombre_tire
+        temp = tableau[i];
+        tableau[i] = tableau[nombre_tire];
+        tableau[nombre_tire]=temp;
+    }
 
-int main() {
+}
+divided_collection divide_tableau(doc * tab) {
+    divided_collection col;
+    srand(time(NULL));
+    col.entrainement = malloc(TAILLE_ENTRAINEMENT * sizeof(doc));
+    col.test = malloc(TAILLE_TEST * sizeof(doc));
+    int *alea = malloc(NB_DOC*sizeof(int));
+    int i = 0;
+    for(i = 0; i< NB_DOC; i++){
+        alea[i]=i;
+    }
+    melanger(alea, NB_DOC);
+    for (int i = 0; i < TAILLE_TEST; i++){
+        col.test[i] = tab[alea[i]];
+    }
+    for (int i = TAILLE_TEST; i < NB_DOC; i++) {
+        col.entrainement[i-TAILLE_TEST] = tab[alea[i]];
+    }
+    return col;
+}
+
+void parse_file(char *name) {
     FILE* base = NULL;
-    base = fopen("BaseReuters-29", "r");
+    base = fopen(name, "r");
     int i = 0;
     if (base != NULL) {
         char *line = malloc(sizeof (char) *TAILLE_VOC);
@@ -68,7 +108,6 @@ int main() {
                 prec = cour;
                 cour = newCell;
                 indVal = strtok_r(NULL, " ", &indVal_end);
-                //printf("%s", indVal);
             }
             prec->suiv = NULL;
             tableau[i] = *newDoc;
@@ -76,10 +115,18 @@ int main() {
             i++;
         }
         fclose(base);
-        //affiche_tableau(tableau);
     } else {
         printf("Erreur lors de l'ouverture du fichier");
     }
+}
 
+int main() {
+    parse_file("BaseReuters-29");
+    affiche_tableau(tableau, NB_DOC);
+    /* le tableau des documents est en variable globale 
+     -> eventuellement à changer */
+    divided_collection col = divide_tableau(tableau);
+    //affiche_tableau(col.test, TAILLE_TEST);
+    //affiche_tableau(col.entrainement, TAILLE_ENTRAINEMENT);
     return 0;
 }
